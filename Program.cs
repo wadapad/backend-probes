a709c69f3a6044540ee0897c611a35d135c6e087
Marat Omaraliev <marat.omaraliev@syngenta.com>
2025-07-09 17:51:48 +0600

    probe!

--- Program.cs
+++ Program.cs
@@ -2,5 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/api/hello", () => "Hello, EDP!");
+app.MapGet("/api/readiness", () => "readiness-ok");
+app.MapGet("/api/liveness", () => "liveness-ok");
 
 app.Run();
